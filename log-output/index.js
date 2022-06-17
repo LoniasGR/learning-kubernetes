27090d2dbf316e17b1f0f1bba6bff8c23e89e038
Leonidas Avdelas <avdelasleonidas@gmail.com>
2022-06-17 14:06:08 +0300

    Current snapshot

--- log-output/index.js
+++ log-output/index.js
@@ -1,7 +1,20 @@
 const crypto = require('crypto');
+const express = require('express');
+
+const app = express();
+const port = 3000;
 
 const uuid = crypto.randomUUID();
-
+let status;
 setInterval(() => {
-  console.log(`${new Date().toISOString()}: ${uuid}`);
-}, 5000)
+  status = `${new Date().toISOString()}: ${uuid}`;
+  console.log(status);
+}, 5000);
+
+app.get('/status', (req, res) => {
+  res.send(status);
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
